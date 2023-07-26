bbcbf18906b40be25665dee1fb11545b07b76b45
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-07-26 19:11:57 +0100

    remove padding from globalStyle

--- src/Assets/Styles/globalStyles.js
+++ src/Assets/Styles/globalStyles.js
@@ -2,7 +2,6 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
 body {
-    padding: 16px;
     background-color: ${({ theme }) => theme.colors.grayish};
     font-family: "Poppins";
 }

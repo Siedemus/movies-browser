5c71d5a593e2e0a2738000ccf37c4f3c3dbf7092
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-07-26 19:09:34 +0100

    remove unnessesary Container from App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
-import { Container } from "./components/Container";
-import { Tile } from "./components/Tile/index";
-
 function App() {
   return (
-    <Container>
-      <Tile
-        title="Mulan"
-        year="2020"
-        infoProduction="China USA"
-        infoDate="24.10.2020"
-        tag="Action"
-        filmDescription="A young Chinese maiden disguises herself as a male warrior in order to
-      save her father. Disguises herself as a male warrior in order to save her
-      father. A young Chinese maiden disguises herself as a male warrior in
-      order to save her father."
-      />
-    </Container>
+
   );
 }
 export default App;

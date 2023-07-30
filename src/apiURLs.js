e9c9f128df37bb2f4ac9b086a2459bf5588b216e
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-07-30 23:31:39 +0100

    ulrs added details and Images

--- src/apiURLs.js
+++ src/apiURLs.js
@@ -1,3 +1,6 @@
 export const images = "https://image.tmdb.org/t/p/w500";
 export const movies = "https://api.themoviedb.org/3/discover/movie";
 export const genres = "https://api.themoviedb.org/3/genre/movie/list";
+export const details_url = "https://api.themoviedb.org/3/movie/";
+export const detailImages_url =
+  "https://api.themoviedb.org/3/collection/{collection_id}/images";

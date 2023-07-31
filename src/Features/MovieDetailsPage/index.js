ce46d65ea2671913cd340cb6992cdb56cd9561e8
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-07-31 23:23:41 +0100

    create Movie detail pages

--- src/Features/MovieDetailsPage/index.js
+++ src/Features/MovieDetailsPage/index.js
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
-import { details_url } from "../../apiURLs";
-import { Container, Header } from "../MoviesPage/styled";
+import { details_url, poster_path } from "../../apiURLs";
 import { useParams } from "react-router-dom";
+import {
+  Panel,
+  Wrap,
+  Container,
+  TileContainer,
+  Header,
+  PosterPath,
+} from "./styled";
+import { Tile } from "../../Common/Tile";
+import { Scores } from "../../Common/Scores/index";
 
-const MovieDetailsPage = () => {
+const MovieDetailsPage = (backdrop_path, title) => {
   const [movie, setMovie] = useState(null);
   const { id } = useParams();
 
@@ -25,11 +34,60 @@ const MovieDetailsPage = () => {
       })
       .catch((err) => console.error("error:" + err));
   }, [id]);
-  return (
-    <Container>
-      <Header>{movie?.original_title}</Header>
-    </Container>
-  );
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDdhNDgyZjBmNGQ2YTYzMjMyZWQ5NDYwNDY5ODc5NyIsInN1YiI6IjY0YzJlYzIzNjZhMGQzMDBlN2Q1M2I1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HucdpQMGjiivmlJCKl9m7QEgqOFwrSHhZLxSIj8tEDM",
+      },
+    };
+
+    fetch(backdrop_path + `${id}`, options)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setMovie(json);
+      })
+      .catch((err) => console.error("error:" + err));
+  }, [id]);
+
+  return movie ? (
+    <>
+      <Wrap>
+        <PosterPath
+          src={`${poster_path}${movie.backdrop_path}`}
+          alt={`${title}`}
+          // poster={movie.backdrop_path}
+        >
+          <Panel>
+            <Container>
+              <Header>{movie.original_title}</Header>
+              <Scores data={{ score: movie.vote_average }} />
+              <Scores data={{ votes: movie.vote_count }} />
+            </Container>
+          </Panel>
+        </PosterPath>
+      </Wrap>
+      <TileContainer key={movie.poster_path}>
+        <Tile
+          id={movie.id}
+          poster={movie.poster_path}
+          title={movie.title}
+          subtitle={movie.release_date}
+          infoProduction={movie.production_countries
+            .map(({ name }) => name)
+            .join(", ")}
+          infoDate={movie.release_date}
+          tags={movie.genre_ids}
+          rate={{ score: movie.vote_average, votes: movie.vote_count }}
+          filmDescription={movie.overview}
+        />
+      </TileContainer>
+    </>
+  ) : null;
 };
 
 export default MovieDetailsPage;
---
+++ src/Features/MovieDetailsPage/styled.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const Wrap = styled.div`
+  background-color: ${({ theme }) => theme.colors.black};
+  height: 770px;
+  margin-bottom: 64px;
+`;
+
+export const Panel = styled.div`
+  height: 770px;
+  width: 1368px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+
+export const PosterPath = styled.div`
+  height: 770px;
+  width: 1368px;
+  margin: auto;
+  background-size: contain;
+  background-image: url(${(props) => props.src});
+  -webkit-box-shadow: inset 0px 0px 100px 50px
+    ${({ theme }) => theme.colors.black};
+  -moz-box-shadow: inset 0px 0px 100px 50px ${({ theme }) => theme.colors.black};
+  box-shadow: inset 0px 0px 100px 50px ${({ theme }) => theme.colors.black};
+`;
+
+export const Container = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+`;
+
+export const TileContainer = styled.div`
+  width: 1368px;
+  margin: auto;
+`;
+
+export const Header = styled.h2`
+  color: ${({ theme }) => theme.colors.white};
+  text-shadow: 0px 0px 20px rgba(0, 0, 0, 1);
+  margin-top: 0px;
+  font-weight: 600;
+  font-size: 64px;
+`;

bdf2beaf3738b82ad695ece7b94937f6ee6dfa06
Bartosz Sakiewa <barsak1177@gmail.com>
2023-08-03 18:52:46 +0200

    add pagination actions and selectors

--- src/Features/MoviesPage/moviesSlice.js
+++ src/Features/MoviesPage/moviesSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   status: "loading",
   data: {},
+  currentPage: 1,
 };
 
 const moviesSlice = createSlice({
@@ -19,13 +20,39 @@ const moviesSlice = createSlice({
     setError: (state) => {
       state.status = "error";
     },
+    firstPage: (state) => {
+      state.currentPage = 1;
+    },
+    previousPage: (state) => {
+      if (state.currentPage !== 1) {
+        state.currentPage--;
+      }
+    },
+    nextPage: (state) => {
+      if (state.currentPage !== 500) {
+        state.currentPage++;
+      }
+    },
+    lastPage: (state) => {
+      state.currentPage = 500;
+    },
   },
 });
 
-export const { fetchMovies, setMovies, setError } = moviesSlice.actions;
+export const {
+  fetchMovies,
+  setMovies,
+  setError,
+  firstPage,
+  previousPage,
+  nextPage,
+  lastPage,
+} = moviesSlice.actions;
 
 const selectMoviesState = (state) => state.movies.data;
 
 export const selectMoviesList = (state) => selectMoviesState(state).results;
+
+export const selectCurrentPage = (state) => state.movies.currentPage;
 
 export default moviesSlice.reducer;

b97216cfc937e54ea36ff2d3846fbe79bb7e3e9e
Bartosz Sakiewa <barsak1177@gmail.com>
2023-08-03 18:53:14 +0200

    ad Pagination

---
+++ src/Features/Pagination/index.js
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Text, Button, Container, Vector, Important } from "./styled";
+import {
+  firstPage,
+  lastPage,
+  nextPage,
+  previousPage,
+  selectCurrentPage,
+  selectTotalPages,
+} from "../MoviesPage/moviesSlice";
+
+export const Pagination = () => {
+  const currentPage = useSelector(selectCurrentPage);
+  const dispatch = useDispatch();
+
+  return (
+    <Container>
+      <Button onClick={() => dispatch(firstPage())}>
+        <Vector alt="first page" />
+        First
+      </Button>
+      <Button onClick={() => dispatch(previousPage())}>
+        <Vector alt="previous page" />
+        Previous
+      </Button>
+      <Text>
+        Page <Important>{currentPage}</Important> of
+        <Important> 500</Important>
+      </Text>
+      <Button next={+true} onClick={() => dispatch(nextPage())}>
+        Next
+        <Vector next={+true} alt="next page" />
+      </Button>
+      <Button next={+true} onClick={() => dispatch(lastPage())}>
+        Last
+        <Vector next={+true} alt="last page" />
+      </Button>
+    </Container>
+  );
+};
---
+++ src/Features/Pagination/styled.js
@@ -0,0 +1,42 @@
+import { styled, css } from "styled-components";
+import { ReactComponent as vector } from "../../Assets/Images/Vector.svg";
+
+export const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.gray};
+  padding: 8px 16px;
+  border: none;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  cursor: pointer;
+
+  ${({ next }) =>
+    next &&
+    css`
+      background-color: ${({ theme }) => theme.colors.blue};
+    `}
+`;
+
+export const Vector = styled(vector)`
+  ${({ next }) =>
+    next &&
+    css`
+      fill: blue;
+      rotate: 180deg;
+    `}
+`;
+
+export const Text = styled.p`
+  color: ${({ theme }) => theme.colors.waterloo};
+  margin: auto 12px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  margin-top: 30px;
+  gap: 12px;
+`;
+
+export const Important = styled.b`
+  color: ${({ theme }) => theme.colors.black};
+`;

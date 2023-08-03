46557b32eef1a15c13791adf5191b0fc0906984f
Bartosz Sakiewa <barsak1177@gmail.com>
2023-08-03 18:54:01 +0200

    apply pagination and center it, move heaer to the left

--- src/Features/MoviesPage/index.js
+++ src/Features/MoviesPage/index.js
@@ -2,38 +2,53 @@ import { useEffect } from "react";
 import { MainTile } from "../../Common/MainTail";
 import { Container, Header, Movie, MovieList } from "./styled";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMovies, selectMoviesList } from "./moviesSlice";
+import {
+  fetchMovies,
+  selectCurrentPage,
+  selectMoviesList,
+} from "./moviesSlice";
 import { fetchGenres } from "../../Common/MainTail/genresSlice";
+import { Pagination } from "../Pagination";
 
 const MoviesPage = () => {
   const dispatch = useDispatch();
+  const currentPage = useSelector(selectCurrentPage);
 
   const moviesData = useSelector(selectMoviesList);
 
   useEffect(() => {
     dispatch(fetchMovies());
+  }, [currentPage]);
+
+  useEffect(() => {
     dispatch(fetchGenres());
   }, []);
 
   return (
-    <Container>
-      <Header>Popular movies</Header>
-      <MovieList>
-        {moviesData
-          ? moviesData.map((movie) => (
-              <Movie key={movie.poster_path}>
-                <MainTile
-                  poster={movie.poster_path}
-                  title={movie.title}
-                  subtitle={movie.release_date}
-                  tags={movie.genre_ids}
-                  rate={{ score: movie.vote_average, votes: movie.vote_count }}
-                />
-              </Movie>
-            ))
-          : null}
-      </MovieList>
-    </Container>
+    <>
+      <Container>
+        <Header>Popular movies</Header>
+        <MovieList>
+          {moviesData
+            ? moviesData.map((movie) => (
+                <Movie key={movie.poster_path}>
+                  <MainTile
+                    poster={movie.poster_path}
+                    title={movie.title}
+                    subtitle={movie.release_date}
+                    tags={movie.genre_ids}
+                    rate={{
+                      score: movie.vote_average,
+                      votes: movie.vote_count,
+                    }}
+                  />
+                </Movie>
+              ))
+            : null}
+        </MovieList>
+        <Pagination />
+      </Container>
+    </>
   );
 };
 
--- src/Features/MoviesPage/styled.js
+++ src/Features/MoviesPage/styled.js
@@ -2,13 +2,17 @@ import { styled } from "styled-components";
 
 export const Container = styled.section`
   max-width: 1400px;
-  margin: 30px auto;
+  margin: 30px auto 130px;
   padding: 0 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const Header = styled.h2`
   font-weight: 600;
   font-size: 36px;
+  align-self: flex-start;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mini}) {
     font-size: 25px;

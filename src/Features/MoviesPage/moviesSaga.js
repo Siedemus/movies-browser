a39ccc7dd60b8ee2770069cc980fd5df58b95b34
Bartosz Sakiewa <barsak1177@gmail.com>
2023-08-03 18:53:04 +0200

    add currentPage handling

--- src/Features/MoviesPage/moviesSaga.js
+++ src/Features/MoviesPage/moviesSaga.js
@@ -1,11 +1,16 @@
-import { delay, call, put, takeEvery } from "redux-saga/effects";
-import { fetchMovies, setMovies, setError } from "./moviesSlice";
+import { delay, call, put, takeEvery, select } from "redux-saga/effects";
+import {
+  fetchMovies,
+  setMovies,
+  setError,
+  selectCurrentPage,
+} from "./moviesSlice";
 import { getMovies } from "./getMovies";
 
 function* fetchMoviesHandler() {
   try {
-    yield delay(2000);
-    const movies = yield call(getMovies);
+    const currentPage = yield select(selectCurrentPage);
+    const movies = yield call(getMovies, currentPage);
     yield put(setMovies(movies));
   } catch {
     yield put(setError);

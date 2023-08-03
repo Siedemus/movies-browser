c1410cce7ebdcb188c30fd7eb8e62fe73a2fdf34
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-08-03 14:58:19 +0100

    clickable logo added

--- src/Assets/Styles/styled.js
+++ src/Assets/Styles/styled.js
@@ -6,13 +6,40 @@ const hoverClassName = "hover";
 
 export const StyledNavLink = styled(NavLink).attrs(() => ({
   activeClassName: activeClassName,
-  hoverClassName: hoverClassName,
 }))`
   text-decoration: none;
   color: ${({ theme }) => theme.colors.black};
 
-  &: ${hoverClassName} {
+  &: hover {
     color: ${({ theme }) => theme.colors.waterloo};
+  }
+
+  &.${activeClassName} {
+    color: ${({ theme }) => theme.colors.stormGray};
+  }
+`;
+
+export const TitleLink = styled(NavLink).attrs(() => ({
+  activeClassName: activeClassName,
+}))`
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 40px;
+  letter-spacing: -1.5px;
+  text-transform: capitalize;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mini}) {
+    font-size: 13px;
+    line-height: 1, 3;
+    letter-spacing: -0.5px;
+    padding-bottom: 0;
+    margin: 0;
   }
 
   &.${activeClassName} {
--- src/Features/Navigation/Logo/index.js
+++ src/Features/Navigation/Logo/index.js
@@ -1,11 +1,13 @@
 import { Wrapper, Title, CameraIcon } from "./styled";
+import { TitleLink } from "../../../Assets/Styles/styled";
 
 export const NavigationLogo = () => {
-
-    return (
-        <Wrapper>
-           <CameraIcon />
-            <Title>Movies Browser</Title>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <TitleLink to="/movie">
+        <CameraIcon />
+        <Title>Movies Browser</Title>
+      </TitleLink>
+    </Wrapper>
+  );
 };

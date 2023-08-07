39fef75b24252d98d798398bb0b31d61c1682c64
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-08-07 22:51:57 +0100

    create const getAuthorizationOption

--- src/apiURLs.js
+++ src/apiURLs.js
@@ -8,3 +8,12 @@ export const detailImages_url =
 
 export const credits_url =
   "https://api.themoviedb.org/3/movie/{movie_id}/credits";
+
+export const getAuthorizationOption = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDdhNDgyZjBmNGQ2YTYzMjMyZWQ5NDYwNDY5ODc5NyIsInN1YiI6IjY0YzJlYzIzNjZhMGQzMDBlN2Q1M2I1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HucdpQMGjiivmlJCKl9m7QEgqOFwrSHhZLxSIj8tEDM",
+  },
+};

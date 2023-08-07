84a3cfec2abf605872d2b46b426499279e4d6832
Oleksandr Vykhor <sashavihor67@gmail.com>
2023-08-07 13:13:47 +0200

    added search

--- src/Features/Navigation/Search/index.js
+++ src/Features/Navigation/Search/index.js
@@ -1,11 +1,37 @@
+import { useDispatch } from "react-redux";
 import { Wrapper, Input } from "./styled";
+import { useRouteMatch } from "react-router-dom/cjs/react-router-dom.min";
+import { useQueryParameter, useReplaceQueryParameter } from "./queryParametrs";
+import { searchQueryParamName } from "./searchQueryParamName";
 
 export const Search = () => {
+    const dispatch = useDispatch();
+    const moviesMatch = useRouteMatch("/movies");
+    const peopleMatch = useRouteMatch("/people");
+    const placeholder = moviesMatch
+        ? "Search for movies..."
+        : "Search for people...";
+
+    const query = useQueryParameter(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameter();
+
+    const onInputChange = ({ target }) => {
+        replaceQueryParameter(
+            searchQueryParamName,
+            target.value.trim() !== "" ? target.value : undefined,
+            peopleMatch ? "people" : undefined,
+            moviesMatch ? "movies" : undefined
+        );
+
+    };
+
     return (
         <>
             <Wrapper>
                 <Input
-                    placeholder="Search for movies..."
+                    placeholder={placeholder}
+                    value={query || ""}
+                    onChange={onInputChange}
                 />
             </Wrapper>
         </>

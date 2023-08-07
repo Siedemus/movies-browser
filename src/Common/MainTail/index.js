1d0613d41bacf29de0fef9be5ac90f72b0c18e50
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-08-07 11:34:23 +0100

    change title nav link to whole MainTail nav link

--- src/Common/MainTail/index.js
+++ src/Common/MainTail/index.js
@@ -18,28 +18,28 @@ export const MainTile = ({ id, title, subtitle, tags, rate, poster }) => {
   const genres = useSelector(selectGenres);
 
   return (
-    <MainTileContainer>
-      <Image src={`${images}${poster}`} alt={`${title} poster`} />
-      <MainTailBox>
-        <Content>
-          <MainTileTitle>
-            <StyledNavLink to={`/movies/${id}`}>{title}</StyledNavLink>
-          </MainTileTitle>
-          <MainTileYear>{subtitle}</MainTileYear>
-          <Tags>
-            {genres && tags ? (
-              tags.map((tag) => (
-                <Tag key={tag}>
-                  {genres.find((genre) => genre.id === tag).name}
-                </Tag>
-              ))
-            ) : (
-              <Tag>No Data</Tag>
-            )}
-          </Tags>
-        </Content>
-        {rate ? <MainTailScores data={rate} /> : null}
-      </MainTailBox>
-    </MainTileContainer>
+    <StyledNavLink to={`/movies/${id}`}>
+      <MainTileContainer>
+        <Image src={`${images}${poster}`} alt={`${title} poster`} />
+        <MainTailBox>
+          <Content>
+            <MainTileTitle>{title}</MainTileTitle>
+            <MainTileYear>{subtitle}</MainTileYear>
+            <Tags>
+              {genres && tags ? (
+                tags.map((tag) => (
+                  <Tag key={tag}>
+                    {genres.find((genre) => genre.id === tag).name}
+                  </Tag>
+                ))
+              ) : (
+                <Tag>No Data</Tag>
+              )}
+            </Tags>
+          </Content>
+          {rate ? <MainTailScores data={rate} /> : null}
+        </MainTailBox>
+      </MainTileContainer>
+    </StyledNavLink>
   );
 };

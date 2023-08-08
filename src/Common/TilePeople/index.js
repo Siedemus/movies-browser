2e3db70618d9eb7a771d9bf3c3ffa8e0ca04a620
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-08-08 23:00:41 +0100

    add TilePeople to the branch

---
+++ src/Common/TilePeople/index.js
@@ -0,0 +1,19 @@
+import {
+    CharacterName,
+    PersonName,
+    Wrapper,
+    GetDetailsPeopleLink,
+  } from "./styled";
+  import { PersonImage } from "./Image";
+  
+  export const PeopleListTile = ({ name, poster, id, character }) => {
+    return (
+      <GetDetailsPeopleLink to={`/people/${id}`} key={id}>
+        <Wrapper>
+          <PersonImage poster={poster} />
+          <PersonName>{name}</PersonName>
+          <CharacterName>{character}</CharacterName>
+        </Wrapper>
+      </GetDetailsPeopleLink>
+    );
+  };

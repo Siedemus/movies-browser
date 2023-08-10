88ee1582736ca27f6631f74a07b8e9efad29d638
Oleksandr Vykhor <sashavihor67@gmail.com>
2023-08-10 11:38:27 +0200

    added store

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ import rootSaga from "./rootSaga";
 import genresSlice from "./Common/MainTail/genresSlice";
 import detailsSlice from "./Features/MovieDetailsPage/detailsSlice";
 import creditsSlice from "./Features/MovieDetailsPage/creditsSlice";
+import peopleSlice from "./Features/PeoplePage/peopleSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -14,6 +15,7 @@ const store = configureStore({
     genres: genresSlice,
     details: detailsSlice,
     credits: creditsSlice,
+    people: peopleSlice,
   },
   middleware: [sagaMiddleware],
 });

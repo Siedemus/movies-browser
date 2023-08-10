5c53323a550f8f3b75558ae6cd1dbf5588f72478
Oleksandr Vykhor <sashavihor67@gmail.com>
2023-08-10 11:38:18 +0200

    aadded root

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -3,7 +3,8 @@ import { moviesSaga } from "./Features/MoviesPage/moviesSaga";
 import { genresSaga } from "./Common/MainTail/genreSaga";
 import { detailsSaga } from "./Features/MovieDetailsPage/detailsSaga";
 import { creditsSaga } from "./Features/MovieDetailsPage/creditsSaga";
+import { watchFetchPeopleList } from "./Features/PeoplePage/peopleSaga";
 
 export default function* rootSaga() {
-  yield all([moviesSaga(), genresSaga(), detailsSaga(), creditsSaga()]);
+  yield all([moviesSaga(), genresSaga(), detailsSaga(), creditsSaga(), watchFetchPeopleList]);
 }

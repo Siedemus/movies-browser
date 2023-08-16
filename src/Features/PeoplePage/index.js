397c2f54743d0435b812c53a1a4a3d44023c9693
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-08-16 19:53:18 +0100

    correct bugs

--- src/Features/PeoplePage/index.js
+++ src/Features/PeoplePage/index.js
@@ -1,58 +1,79 @@
 import { Container, Header, ItemPeopleList, PeopleList } from "./styled";
 import { PeopleListTile } from "../../Common/TilePeople/index";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
-    firstPeoplePage,
-    lastPeoplePage,
-    nextPeoplePage,
-    previousPeoplePage,
-    selectCurrentPeoplePage,
-    selectPeopleList,
-    selectStatus
+  fetchPeopleListLoad,
+  firstPeoplePage,
+  lastPeoplePage,
+  nextPeoplePage,
+  previousPeoplePage,
+  selectCurrentPeoplePage,
+  selectPeopleList,
+  selectStatus,
+  setPeoplePageByQuery,
 } from "./peopleSlice";
-import { LoaderContainer } from "../../Common/LoaderContainer/styled"
+import { LoaderContainer } from "../../Common/LoaderContainer/styled";
 import { MoonLoader } from "react-spinners";
 import { ErrorPage } from "../../Common/ErrorPage";
 import { Pagination } from "../../Common/Pagination/index";
+import {
+  useHistory,
+  useLocation,
+} from "react-router-dom/cjs/react-router-dom.min";
+import { useEffect } from "react";
 
 const PeoplePage = () => {
-    const currentPeoplePage = useSelector(selectCurrentPeoplePage);
-    const popularPeople = useSelector(selectPeopleList);
-    const peopleStatus = useSelector(selectStatus);
+  const dispatch = useDispatch();
+  const currentPeoplePage = useSelector(selectCurrentPeoplePage);
+  const popularPeople = useSelector(selectPeopleList);
+  const peopleStatus = useSelector(selectStatus);
 
+  const location = useLocation();
+  const history = useHistory();
+  const query = new URLSearchParams(location.search).get("page");
+
+  useEffect(() => {
+    dispatch(setPeoplePageByQuery(parseInt(query)));
+  }, [query, dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchPeopleListLoad());
+    history.replace(`${location.pathname}?page=${currentPeoplePage}`);
+  }, [currentPeoplePage, dispatch, history, location.pathname]);
+
+  return (
     <Container>
-        <Header>Popular People</Header>
-        <PeopleList>
-            {peopleStatus === "success" ? (
-                <>
-                    {popularPeople.map((people) => (
-                        <ItemPeopleList key={people.id}>
-                            <PeopleListTile
-                                id={people.id}
-                                poster={people.profile_path}
-                                name={people.name}
-                            />
-                        </ItemPeopleList>
-                    ))}
-                    <Pagination
-                        currentPage={currentPeoplePage}
-                        firstPage={firstPeoplePage}
-                        previousPage={previousPeoplePage}
-                        nextPage={nextPeoplePage}
-                        lastPage={lastPeoplePage}
-                    />
-                </>
-            ) : peopleStatus === "loading" ? (
-                <LoaderContainer>
-                    <MoonLoader color="#18181B" size={80} />
-                </LoaderContainer>
-            ) : peopleStatus === "error" ? (
-                <ErrorPage />
-            ) : null}
-        </PeopleList>
+      <Header>Popular People</Header>
+      <PeopleList>
+        {peopleStatus === "success" ? (
+          <>
+            {popularPeople.map((people) => (
+              <ItemPeopleList key={people.id}>
+                <PeopleListTile
+                  id={people.id}
+                  poster={people.profile_path}
+                  name={people.name}
+                />
+              </ItemPeopleList>
+            ))}
+            <Pagination
+              currentPage={currentPeoplePage}
+              firstPage={firstPeoplePage}
+              previousPage={previousPeoplePage}
+              nextPage={nextPeoplePage}
+              lastPage={lastPeoplePage}
+            />
+          </>
+        ) : peopleStatus === "loading" ? (
+          <LoaderContainer>
+            <MoonLoader color="#18181B" size={80} />
+          </LoaderContainer>
+        ) : peopleStatus === "error" ? (
+          <ErrorPage />
+        ) : null}
+      </PeopleList>
     </Container>
-
-
-}
+  );
+};
 
 export default PeoplePage;
--- src/Features/PeoplePage/peopleSlice.js
+++ src/Features/PeoplePage/peopleSlice.js
@@ -1,70 +1,72 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    people: [],
-    currentPage: 1,
-    status: "loading",
-
+  people: [],
+  currentPage: 1,
+  status: "loading",
 };
 
 const peopleSlice = createSlice({
-    name: "people",
-    initialState,
-    reducers: {
-        fetchPeopleListLoad: (state) => {
-            state.status = "loading";
-        },
-        fetchPeopleListSuccess: (state, { payload }) => {
-            state.people = payload;
-            state.status = "success";
-        },
-        fetchPeopleListError: (state) => {
-            state.status = "error";
-        },
-        firstPeoplePage: (state) => {
-            state.currentPage = 1;
-        },
-        previousPeoplePage: (state) => {
-            if (state.currentPage !== 1) {
-                state.currentPage--;
-            }
-        },
-        nextPeoplePage: (state) => {
-            if (state.currentPage !== 500) {
-                state.currentPage++;
-            }
-        },
-        lastPeoplePage: (state) => {
-            state.currentPage = 500;
-        },
-        setPeoplePageByQuery: (state, { payload }) => {
-            if (isNaN(payload)) {
-                state.currentPage = 1;
-            } else if (payload > 500) {
-                state.currentPage = 500;
-            } else if (payload < 1) {
-                state.currentPage = 1;
-            } else {
-                state.currentPage = payload;
-            }
-        },
+  name: "people",
+  initialState,
+  reducers: {
+    fetchPeopleListLoad: (state) => {
+      state.status = "loading";
     },
+    fetchPeopleListSuccess: (state, { payload }) => {
+      state.people = payload;
+      state.status = "success";
+    },
+    fetchPeopleListError: (state) => {
+      state.status = "error";
+    },
+    firstPeoplePage: (state) => {
+      state.currentPage = 1;
+    },
+    previousPeoplePage: (state) => {
+      if (state.currentPage !== 1) {
+        state.currentPage--;
+      }
+    },
+    nextPeoplePage: (state) => {
+      if (state.currentPage !== 500) {
+        state.currentPage++;
+      }
+    },
+    lastPeoplePage: (state) => {
+      state.currentPage = 500;
+    },
+    setPeoplePageByQuery: (state, { payload }) => {
+      if (isNaN(payload)) {
+        state.currentPage = 1;
+      } else if (payload > 500) {
+        state.currentPage = 500;
+      } else if (payload < 1) {
+        state.currentPage = 1;
+      } else {
+        state.currentPage = payload;
+      }
+    },
+  },
 });
 
 export const {
-    fetchPeopleListLoad,
-    fetchPeopleListError,
-    fetchPeopleListSuccess,
-    firstPeoplePage,
-    previousPeoplePage,
-    nextPeoplePage,
-    lastPeoplePage,
-    setPeoplePageByQuery,
+  fetchPeopleListLoad,
+  fetchPeopleListError,
+  fetchPeopleListSuccess,
+  firstPeoplePage,
+  previousPeoplePage,
+  nextPeoplePage,
+  lastPeoplePage,
+  setPeoplePageByQuery,
 } = peopleSlice.actions;
 
 export const selectPeopleState = (state) => state.people;
 export const selectStatus = (state) => selectPeopleState(state).status;
-export const selectPeopleList = (state) => selectPeopleState(state).people.results;
-export const selectCurrentPeoplePage = (state) => state.movies.currentPage;
+
+export const selectPeopleList = (state) =>
+  selectPeopleState(state).people.results;
+
+export const selectCurrentPeoplePage = (state) => state.people.currentPage;
 
 export default peopleSlice.reducer;
